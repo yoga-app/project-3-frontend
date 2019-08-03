0d0ac8bb5e6b47d90a0eef14bf3c5e3d1e464926
Taluama Velludo <taluama@gmail.com>
2019-08-03 13:58:53 -0400

    done

--- src/components/classes/Classes.js
+++ src/components/classes/Classes.js
@@ -4,10 +4,28 @@ import './classes.css';
 class Classes extends Component {
   render() {
     return (
-      <div className="temporary">
-        <small>[this is the <b>classes component</b>]</small><br />
-        <small>[<b>schedule</b> and <b>rates</b> go here]</small>
-
+      <div className="classes">
+        {/* <small>[<b>schedule</b> and <b>rates</b> go here]</small> */}
+        <ul>
+          <li><h4>Prices:</h4></li>
+          <li>
+            <p>single class</p>
+            <p>$10</p>
+          </li>
+          <li>
+            <p>10 classes</p>
+            <p>$90</p>
+          </li>
+          <li>
+            <p>25 classes</p>
+            <p>$220</p>
+          </li>
+          <li>
+            <p>50 classes</p>
+            <p>$450</p>
+          </li>
+        </ul>
+        <p className="off">Get 20% OFF one class for every friend you bring!<br />Earn a $5 Credit after 15 visits!</p>
       </div>
     );
   }
