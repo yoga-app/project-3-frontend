0e8809fd63ba180cc772682c406545693770781b
arstrel <arstrel@gmail.com>
2019-08-07 12:35:06 -0400

    routine builder in progress

--- src/components/daily-routine/DailyRoutine.js
+++ src/components/daily-routine/DailyRoutine.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import './dailyroutine.css';
 
 class DailyRoutine extends Component {
+  showRoutines() {
+    return this.props.currentUser.daily.routine.map(eachR=>{
+      return (
+        <div key={eachR._id} className="each-asana-wrapper">
+          <img src={eachR.img_url} alt="asana" className="daily-asana"/>
+          <p>{eachR.english_name}</p>
+          <p>{eachR.sanskrit_name}</p>
+        </div>
+      )
+    })
+  }
+
   render() {
     return (
       <div className="temporary">
-        <small>[this is the <b>Daily Routine component</b>]</small><br />
-        
+        <small>[this is the <b>connected</b> Daily Routine component]</small><br />
+        <h5>Short routine for today:</h5>
+        <p>Description: {this.props.currentUser.daily.description}</p>
+        <div className="daily-asanas-wrapper">
+          {this.showRoutines()}
+        </div>
       </div>
     );
   }
--- src/components/routine-builder/RoutineBuilder.js
+++ src/components/routine-builder/RoutineBuilder.js
@@ -6,18 +6,53 @@ class RoutineBuilder extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      routine: [],
+      description: '',
     }
   }
-  
+  showAllAsanas() {
+    return this.props.asanas.map(eachA => {
+      return (
+        <div key={eachA._id} className="each-asana-builder-wrapper">
+          <img src={eachA.img_url} alt="asana" className="daily-builder-asana"/>
+          <p>{eachA.english_name}</p>
+          <p>{eachA.sanskrit_name}</p>
+        </div>
+      )
+    })
+  }
 
+  handleInput = (e) => {
+    this.setState({description: e.target.value})
+  }
+
+  handleSubmit = () => {
+    axios.post('http://localhost:5000/api/auth/update-daily-routine-for-all/', this.state)
+    .then(response=> {
+      console.log(response.date);
+    })
+    .catch(err=> {
+      console.log(err);
+    })
+  }
 
 
   render() {
     return (
       <div className="temporary">
         <small>[this is the <b>Routine Builder component</b>]</small><br />
-        
+        <div className="form-wrapper">
+          <form onSubmit={this.handleSubmit}>
+            <input name="description" onChange={this.handleInput} value={this.state.description} />
+            <button>Save</button>
+          </form>
+        </div>
+        <div className="asanas-list-builder-wrapper">
+          {this.showAllAsanas()}
+        </div>
+        <div className="daily-asanas-builder-wrapper">
+
+        </div>
       </div>
     );
   }
